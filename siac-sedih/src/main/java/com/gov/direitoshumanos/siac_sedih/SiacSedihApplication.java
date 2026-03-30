f89a0e1d93ecbb8b17ccbd0940c0303022392932
Yago Moreira <yagomoreira_dev@yahoo.com>
2026-03-30 09:48:33 -0300

    Iniciando projeto nova api-siac

---
+++ siac-sedih/src/main/java/com/gov/direitoshumanos/siac_sedih/SiacSedihApplication.java
@@ -0,0 +1,13 @@
+package com.gov.direitoshumanos.siac_sedih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiacSedihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiacSedihApplication.class, args);
+	}
+
+}
